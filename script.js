dfb0c1a1f7c0c6728e6f00cf9b8c8d7b7cea4ab3
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-09-03 21:25:45 +0800

    v1.0.1

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 console.log("Hello There :0");
 
-const fileInput = document.getElementById("fileInput");
-const fileUpload = document.getElementById("fileUpload");
-const extErrMessage = document.getElementById("extensionError");
-const storageDownloader = document.getElementById("storageDownloader");
-const uploadForm = document.getElementById("uploadForm");
+const fileInputBox = document.getElementById("fileInputBox");
+const fileInputButton = document.getElementById("fileInputButton");
+const extensionErrorMessage = document.getElementById("extensionErrorMessage");
 const encryptFileBox = document.getElementById("encryptFileBox");
+const storageDownloaderBox = document.getElementById("storageDownloaderBox");
+const storageDownloadButton = document.getElementById("storageDownloadButton");
+const uploadForm = document.getElementById("uploadForm");
 
-fileInput.addEventListener("input", function (e) {
-  var splitFileName = e.target.value.split(".");
-  if (splitFileName.length < 3) {
-    fileUpload.disabled = false;
-    extErrMessage.setAttribute("style", "opacity: 0;");
-  } else {
-    fileUpload.disabled = true;
-    extErrMessage.setAttribute(
-      "style",
-      "opacity: 100; color: red; font-size: 10;"
-    );
-  }
+fileInputBox.addEventListener("input", function(e) {
+    var splitFileName = e.target.value.split(".");
+    if (splitFileName.length < 3) {
+      fileInputButton.disabled = false;
+        extensionErrorMessage.setAttribute("style", "opacity: 0;");
+    } else {
+      fileInputButton.disabled = true;
+        extensionErrorMessage.setAttribute("style",
+            "opacity: 100; color: red; font-size: 10;"
+        );
+    }
 });
 
-storageDownloader.addEventListener("keypress", function (event) {
-  if (event.key == "Enter") {
-    window.location.href = `https://Main-Charlzk-Server.charlzk.repl.co/download?filename=${storageDownloader.value}`;
-  }
+storageDownloaderBox.addEventListener("keypress", function(event) {
+    if (event.key == "Enter") {
+        window.location.href = `https://Main-Charlzk-Server.charlzk.repl.co/download?filename=${storageDownloaderBox.value}`;
+    }
 });
 
 function encryptionBox() {
-  if (encryptFileBox.checked == true) {
-    uploadForm.setAttribute(
-      "action",
-      "https://Main-Charlzk-Server.charlzk.repl.co/upload"
-    );
-  } else {
-    uploadForm.setAttribute(
-      "action",
-      "https://Main-Charlzk-Server.charlzk.repl.co/uploadnotencrypted"
-    );
-  }
+    if (encryptFileBox.checked == true) {
+        uploadForm.setAttribute(
+            "action",
+            "https://Main-Charlzk-Server.charlzk.repl.co/upload"
+        );
+    } else {
+        uploadForm.setAttribute(
+            "action",
+            "https://Main-Charlzk-Server.charlzk.repl.co/uploadnotencrypted"
+        );
+    }
 }
 
-function storageDLClick() {
-  window.location.href = `https://Main-Charlzk-Server.charlzk.repl.co/download?filename=${storageDownloader.value}`;
-}
+storageDownloadButton.addEventListener("click", function() {
+    window.location.href = `https://Main-Charlzk-Server.charlzk.repl.co/download?filename=${storageDownloaderBox.value}`;
+});

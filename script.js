1dc113c3b1e7662d2b0195a40aa87998b77179ec
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-09-04 12:15:06 +0000

    Change water.css folder to waterLib

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ const encryptFileBox = document.getElementById("encryptFileBox");
 const themeBox = document.getElementById("themeBox");
 
 fileInputBox.addEventListener("input", function(e) {
-    var splitFileName = e.target.value.split(".");
-    if (splitFileName.length < 3) {
-      fileInputButton.disabled = false;
-        extensionErrorMessage.setAttribute("style", "opacity: 0;");
-    } else {
-      fileInputButton.disabled = true;
-        extensionErrorMessage.setAttribute("style", "opacity: 100; color: red; font-size: 10;");
-    }
+  var splitFileName = e.target.value.split(".");
+  if (splitFileName.length < 3) {
+    fileInputButton.disabled = false;
+    extensionErrorMessage.setAttribute("style", "opacity: 0;");
+  } else {
+    fileInputButton.disabled = true;
+    extensionErrorMessage.setAttribute("style", "opacity: 100; color: red; font-size: 10;");
+  }
 });
 
 encryptFileBox.addEventListener("click", function() {
-    if (encryptFileBox.checked == true) {
-        uploadForm.setAttribute("action", "https://Main-Charlzk-Server.charlzk.repl.co/upload");
-    } else {
-        uploadForm.setAttribute("action", "https://Main-Charlzk-Server.charlzk.repl.co/uploadnotencrypted");
-    }
+  if (encryptFileBox.checked == true) {
+    uploadForm.setAttribute("action", "https://Main-Charlzk-Server.charlzk.repl.co/upload");
+  } else {
+    uploadForm.setAttribute("action", "https://Main-Charlzk-Server.charlzk.repl.co/uploadnotencrypted");
+  }
 });
 
 themeBox.addEventListener("click", function() {
-    if (themeBox.checked == true) {
-        mainCSS.setAttribute("href", "./water.css/dark.css");
-    } else {
-        mainCSS.setAttribute("href", "./water.css/light.css");
-    }
+  if (themeBox.checked == true) {
+    mainCSS.setAttribute("href", "./waterLib/dark.css");
+  } else {
+    mainCSS.setAttribute("href", "./waterLib/light.css");
+  }
 });
